ccae45d2dfb9c51a0c9269dcb6f3eaee55430ec9
Jefferson Cunha <jeffersondasilva14@gmail.com>
2016-04-17 11:57:01 -0300

    Ajuste de json
    Signed-off-by: Jefferson Cunha <jeffersondasilva14@gmail.com>

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -27,6 +27,6 @@ class Api::V1::UsersController < ApplicationController
 	private
 
 	def user_params
-		params.require(:user).permit(:email, :password)
+		params.require(:user).permit(:nome, :email, :password, :password_confirmation)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  match '*any' => 'application#options', :via => [:options]
   #Creating routes to API
   namespace :api, defaults: {format: :json} do
     scope module: :v1 do
